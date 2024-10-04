3d1abe2bba37e97f09fbbae22f4d13f62f42e55f
Griotold <gotjd9773@naver.com>
2024-10-04 11:36:50 +0900

    SRP(Single Responsibility Principle) - 단일 책임 원칙 1. GameApplication - 프로그램의 진입점을 담당하는 역할을 분리하기 2. ConsoleInputHandler, ConsoleOutputHandler - 입출력을 담당하는 역할 분리하기 3. GameBoard - 보드판에 "정중하게" 물어보기

---
+++ src/main/java/cleancode/minesweeper/tobe/GameApplication.java
@@ -0,0 +1,10 @@
+package cleancode.minesweeper.tobe;
+
+public class GameApplication {
+
+    // 다른 게임도 실행해 볼 수도 있겠다!
+    public static void main(String[] args) {
+        Minesweeper minesweeper = new Minesweeper();
+        minesweeper.run();
+    }
+}
